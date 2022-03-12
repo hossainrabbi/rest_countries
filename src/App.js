3c6504cd5c68df715ea0ef9c7fe5c7f560ab8b1a
Hossain Rabbi <hossainrabbi.dev@gmail.com>
2022-03-12 06:37:54 +0600

    make navbar with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Countries from './components/Countries';
+import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <div>
+    <Layout>
       <Countries />
-    </div>
+    </Layout>
   );
 }
